26aee7d96f5820e047fe47ec05a79e2c2d3fdc5e
leesahanders <lisamaeanders@gmail.com>
2022-07-21 19:57:37 +0000

    Syncing changes

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -20,12 +20,32 @@ test_that("{shinytest2} recording: learn_shinytest2", {
 })
 
 
-test_that("{shinytest2} recording: test2", {
-  app <- AppDriver$new(name = "test2", height = 648, width = 1011, variant= platform_variant())
-  withr::defer({app$stop()})
-  app$set_inputs(end_date = "2018-05-18", api = "Keytruda")
-  app$click("submit")
-  app$wait_for_idle()
-  app$expect_values()
-  app$expect_values(output = "tble")
-})
+# test_that("{shinytest2} recording: test2", {
+#   app <- AppDriver$new(name = "test2", height = 648, width = 1011, variant= platform_variant())
+#   withr::defer({app$stop()})
+#   app$set_inputs(end_date = "2018-05-18", api = "Keytruda")
+#   app$click("submit")
+#   app$wait_for_idle()
+#   app$expect_values()
+#   app$expect_values(output = "tble")
+# })
+# 
+# test_that("{shinytest2} recording: learn_shinytest2_test", {
+#   app <- AppDriver$new(name = "learn_shinytest2_test", height = 810, width = 742)
+#   app$set_inputs(api = "tyl")
+#   app$set_inputs(api = "tyle")
+#   app$set_inputs(api = "tyleno")
+#   app$set_inputs(api = "tylenol")
+#   app$click("submit")
+#   app$expect_values()
+# })
+# 
+# 
+# test_that("{shinytest2} recording: learn_shinytest2_test2", {
+#   app <- AppDriver$new(name = "learn_shinytest2_test2", height = 833, width = 629)
+#   app$set_inputs(api = "")
+#   app$set_inputs(api = "advil")
+#   app$click("submit")
+#   app$expect_values()
+#   app$expect_values()
+# })
