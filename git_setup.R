db8e5c52e43b3309b610d55c45bac23190b11bff
leesahanders <lisa.anders@posit.co>
2023-02-27 17:04:47 +0000

    Initial commit

--- git_setup.R
+++ git_setup.R
@@ -2,7 +2,8 @@ library(usethis)
 library(gitcreds)
 
 
-usethis::use_git_config(user.name = "leesahanders", user.email = "lisamaeanders@gmail.com")
+# usethis::use_git_config(user.name = "leesahanders", user.email = "lisamaeanders@gmail.com")
+usethis::use_git_config(user.name = "leesahanders", user.email = "lisa.anders@posit.co")
 usethis::create_github_token()
 gitcreds::gitcreds_set()
 
--- githubactions.R
+++ githubactions.R
@@ -1,4 +1,6 @@
 #basic github 
-usethis::use_github_action(url = "https://raw.githubusercontent.com/rstudio/actions/main/examples/connect-publish.yaml")
+# usethis::use_github_action(url = "https://raw.githubusercontent.com/rstudio/actions/main/examples/connect-publish.yaml")
+# 
+# usethis::use_github_action(url="https://raw.githubusercontent.com/rstudio/shinytest2/main/.github/workflows/test-actions.yaml")
 
-usethis::use_github_action(url="https://raw.githubusercontent.com/rstudio/shinytest2/main/.github/workflows/test-actions.yaml")
+usethis::use_github_action(url = "https://raw.githubusercontent.com/rstudio/actions/main/examples/test-r-lib.yaml")
